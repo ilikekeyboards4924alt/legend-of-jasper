36934fbe88a80b8a9c8db90f9737c9b44a083c22
Epic Gamer <trevorfults69@gmail.com>
2025-04-17 20:48:16 -0500

    fix build branch

--- build/index.js
+++ build/index.js
@@ -23,7 +23,7 @@ let button = new StartButton();
 button.init(renderer.canvas.width / 2 - 128 / 2, renderer.canvas.height / 2 - 128 / 2, 128, 64);
 button.initAnimation(startButtonAnimation);
 let player = new Player();
-player.init(0, 0, 96 * 2 / 3, 243 * 2 / 3);
+player.init(0, 0, 96 * 3 / 3, 243 * 3 / 3);
 player.initAnimation(walkCycleAnimation);
 function main() {
     renderer.ctx.fillRect(0, 0, renderer.canvas.width, renderer.canvas.height);
@@ -38,9 +38,9 @@ function main() {
     if (gameData.state == 0) {
         if (gameData.frameCounter >= 0)
             titleCard.visible = true;
-        if (gameData.frameCounter > 60)
-            titleCard.visible = false;
         if (gameData.frameCounter > 120)
+            titleCard.visible = false;
+        if (gameData.frameCounter > 130)
             gameData.state = 1;
     }
     if (gameData.state == 1) {
