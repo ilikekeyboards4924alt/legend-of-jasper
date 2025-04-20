6155903ebcda0704cc02450d69d986f0413bba14
Epic Gamer <trevorfults69@gmail.com>
2025-04-19 23:23:53 -0500

    build camera.js

--- build/core/camera.js
+++ build/core/camera.js
@@ -8,8 +8,6 @@ export class Camera {
     center(rect) {
         this.offset.x = lerp(this.offset.x, rect.x + Math.floor(rect.w / 2) - Math.floor(this.renderer.canvas.width / 2));
         this.offset.y = lerp(this.offset.y, rect.y + Math.floor(rect.h / 2) - Math.floor(this.renderer.canvas.height / 2));
-        // this.offset.x = rect.x + Math.floor(rect.w/2) - Math.floor(this.renderer.canvas.width/2);
-        // this.offset.y = rect.y + Math.floor(rect.h/2) - Math.floor(this.renderer.canvas.height/2);
         // clamps the player's camera between the world's 4 borders
         // this.offset.x = Math.min(Math.max(world.border.left, this.offset.x), world.border.right - this.renderer.width);
         // this.offset.y = Math.min(Math.max(world.border.top, this.offset.y), world.border.bottom - this.renderer.height);
